34a78d1472f7161a14841e9ac39606dfc09c8f0a
Camyul <vankata.lt@outlook.com>
2015-10-08 00:14:36 +0300

    Ending 4 problem

---
+++ Introduction to Programming/2.PrintYourName/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2.PrintMyName
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Whats your First Name: ");
+            string FirstName = Console.ReadLine();
+            Console.Write("Whats your Last Name: ");
+            string LastName = Console.ReadLine();
+            Console.WriteLine("Your Full Name is: " +FirstName +" "+ LastName);
+	{
+		
+	}
+        }
+    }
+}
---
+++ Introduction to Programming/4.HelloCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4.HelloCSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello C#");
+        }
+    }
+}
