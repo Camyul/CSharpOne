e6fabd4c9dab68aab74a3e07a997709af8dd7780
Camyul <vankata.lt@outlook.com>
2015-10-07 23:33:32 +0300

    First Console Application

---
+++ Introduction to Programming/1.Play with Visual Studio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1.Play_with_Visual_Studio
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
