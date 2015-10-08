eb0e26cbea8464d76ab97bb56b7240bd23132c77
Camyul <vankata.lt@outlook.com>
2015-10-08 15:05:27 +0300

    Ending 10.Problem

---
+++ Introduction to Programming/FirstLoop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int b = 1;
+        for (int i = 2; i < 12; i++)
+        {
+            Console.WriteLine(b * i);
+            b = b * (-1);
+        }
+    }
+}
---
+++ Introduction to Programming/PrintMyFirstAndLastName/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Your First Name is: ");
+            string FirstName = Console.ReadLine();
+            Console.Write("Your Last Name is: ");
+            string LastName = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine(FirstName);
+            Console.WriteLine(LastName);
+        }
+    }
