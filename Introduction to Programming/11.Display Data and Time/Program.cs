99a063e86af71a89e95cf501f7b43d6ea7f65e4a
Camyul <vankata.lt@outlook.com>
2015-10-09 14:41:16 +0300

    Problem 15 is Eliminated

---
+++ Introduction to Programming/11.Display Data and Time/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+    class Display_Data_and_Time
+    {
+        static void Main()
+        {
+            DateTime DateValue = DateTime.Now;
+            Console.WriteLine("Date and Time with Milliseconds: {0}",
+                           DateValue.ToString("dd/MM/yyyy HH:mm:ss.fff tt"));
+        }
+    }
+
---
+++ Introduction to Programming/15.Age after 10 Years/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("When are you born: ");
+            DateTime DateBorn = DateTime.Parse(Console.ReadLine());
+            DateTime DateNow = DateTime.Parse(DateTime.Now.ToString("dd/MM/yyyy"));
+            int DateDiff = DateNow.Year - DateBorn.Year;
+            if  (DateNow.Month < DateBorn.Month)
+            {
+                DateDiff--;
+            }
+            if (DateNow.Month == DateBorn.Month)
+            {
+                if (DateNow.Day < DateBorn.Day)
+                {
+                    DateDiff--;
+                }
+            }
+            if (DateNow.Day == DateBorn.Day)
+            {
+                Console.WriteLine("Happy Birthday!!");
+            }
+            Console.WriteLine("Now you are: {0}", (DateDiff));
+            Console.WriteLine("After 10 years you wiil be a: {0}", DateDiff + 10);
+        }
+    }
